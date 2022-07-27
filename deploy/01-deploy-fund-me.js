e18a57f92d9a4a1d552d214ebb6465d389d26b8f
Hudsen <hudsend@LAPTOP-EF1H27FV.localdomain>
2022-07-27 12:30:26 -0500

    First commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,45 @@
+// function deployFunc() {
+//     console.log("hi!")
+// }
+
+// module.exports.default = deployFunc
+
+const { networkConfig, developmentChains } = require("../helper-hardhat-config")
+const { network } = require("hardhat")
+const { verify } = require("../utils/verify")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log, get } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+
+    //const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    let ethUsdPriceFeedAddress
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await get("MockV3Aggregator")
+        ethUsdPriceFeedAddress = ethUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+    }
+
+    //if the contract doesnt exist, we deploy a minimal version for local testing
+
+    //well what happens when we want to change chains?
+    //when going for localhost or hardhat network we want to use a mock
+    const args = [ethUsdPriceFeedAddress]
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: args, //put pricefeed address
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, args)
+    }
+    log("-------------------------------------------------------")
+}
+module.exports.tags = ["all", "fundme"]
---
+++ test/staging/FundMe.staging.test.js
@@ -0,0 +1,25 @@
+const { getNamedAccounts, ethers, network } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
+const { assert } = require("chai")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async function () {
+          let fundMe
+          let deployer
+          const sendValue = "50000000000000000" //ethers.utils.parseEther("1")
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+
+          it("allows people to fund and withdraw", async function () {
+              await fundMe.fund({ value: sendValue })
+              await fundMe.withdraw()
+              //should add in a wait here since etherscan doesnt update fast enough
+              const endingBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              assert.equal(endingBalance.toString(), "0")
+          })
+      })
